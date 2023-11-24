5883297588e5dfad62d956ecae2efc71275bba58
aziz tariq <ksa.xo1999@gmail.com>
2023-11-24 23:50:02 +0000

    changes were made to activity_standard_cipher.xml, added a new menu for the drawer, added a new navigation for the drawer, made some changes to MainActivity.java stuck on error

--- app/src/main/java/uk/bradford/app_project/MainActivity.java
+++ app/src/main/java/uk/bradford/app_project/MainActivity.java
@@ -1,14 +1,53 @@
 package uk.bradford.app_project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_standard_cipher);
+
+        // Initial DrawerLayout ViewNav
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        // NaviItemSelectedListener was set
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                // Handle navigation view
+                switch (item.getItemId()) {
+                    case R.id.nav_first_item:
+
+                        break;
+                    case R.id.nav_second_item:
+
+                        break;
+                    // Add more cases
+                }
+
+                // Close the navigation drawer
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
