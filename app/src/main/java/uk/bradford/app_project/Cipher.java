3fafd5e4e61af1e5df2dad4e3a90adf0f7bc4960
AlexFW <peherstorfer.alexander@gmail.com>
2023-11-28 13:50:59 +0000

    Vigenere should be working now, added signature for XORCipher

--- app/src/main/java/uk/bradford/app_project/Cipher.java
+++ app/src/main/java/uk/bradford/app_project/Cipher.java
@@ -1,5 +1,5 @@
 package uk.bradford.app_project;
 
 public enum Cipher {
-    VIGENERE
+    VIGENERE, XOR
 }
--- app/src/main/java/uk/bradford/app_project/Crypto.java
+++ app/src/main/java/uk/bradford/app_project/Crypto.java
@@ -1,6 +1,5 @@
 package uk.bradford.app_project;
 
-import android.util.SparseLongArray;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -31,6 +30,9 @@ public class Crypto {
             case VIGENERE:
                 alphabet = Crypto.alphabet1;
                 break;
+            case XOR:
+                // WIth XOR, everything is converted to its binary representation, hence no alphabet check needed
+                return true;
             default:
                 throw new IllegalArgumentException("Given cipher does not exist or corresponding alphabet has not yet been implemented");
         }
@@ -47,9 +49,11 @@ public class Crypto {
         switch (cipher) {
 
             case VIGENERE:
-                checkAlphabet(cipher, plaintext);
-                checkAlphabet(cipher, key);
+                checkAlphabet(cipher, plaintext + key);
                 return encryptVigenere(plaintext, key);
+            case XOR:
+                checkAlphabet(cipher, plaintext + key);
+                return encryptXOR(plaintext, key);
 
             default:
                 throw new IllegalArgumentException("Given Cipher does not exist or is not implemented (Encryption)");
@@ -62,9 +66,11 @@ public class Crypto {
         switch (cipher) {
 
             case VIGENERE:
-                checkAlphabet(cipher, ciphertext);
-                checkAlphabet(cipher, key);
+                checkAlphabet(cipher, ciphertext + key);
                 return decryptVigenere(ciphertext, key);
+            case XOR:
+                checkAlphabet(cipher, ciphertext + key);
+                return decryptXOR(ciphertext, key);
 
             default:
                 throw new IllegalArgumentException("Given Cipher does not exist or is not implemented (Decryption)");
@@ -77,33 +83,89 @@ public class Crypto {
     private static String encryptVigenere(String plaintext, String key) {
 
         // make the key as long as the plaintext
-        if(key.length() > plaintext.length()){
+        if (key.length() > plaintext.length()) {
 
-        }
-        else if (key.length() < plaintext.length()){
+        } else if (key.length() < plaintext.length()) {
 
-            while (key.length() < plaintext.length()){
+            while (key.length() < plaintext.length()) {
                 key += key;
             }
         }
 
         // Creates 2 int arrays that contain the index of each letter in the alphabet
-        int[] keyArr = key.chars().limit(plaintext.length()).map(c -> c -= 65).toArray();
-        int[] plaintextArr = plaintext.chars().map(c -> c -= 65).toArray();
+        int[] keyArr = key.chars().limit(plaintext.length()).map(c -> c - 64).toArray();
+        int[] plaintextArr = plaintext.chars().map(c -> c - 64).toArray();
 
         int[] out = new int[plaintextArr.length];
         // Here, the main encryption happens
-        for(int i = 0; i < plaintext.length(); i++)
-            out[i] = plaintextArr[i] + keyArr[i];
+        for (int i = 0; i < plaintext.length(); i++)
+            out[i] = plaintextArr[i] + keyArr[i] % 27; // to stay in the alphabet range
 
-        return Arrays.stream(out).map(c -> c += 65).map(c -> (char) c).toArray().toString();
 
+        return Arrays.stream(out)
+                .map(c -> c + 64) // mapping back to the ASCII representation
+                .mapToObj(c -> (char) c) // casting back to character
+                .map(String::valueOf) // maps every character to a String object
+                .collect(Collectors.joining()); // joins the string
     }
 
     private static String decryptVigenere(String ciphertext, String key) {
 
-        return null;
+        // make the key as long as the plaintext
+        if (key.length() > ciphertext.length()) {
 
+        } else if (key.length() < ciphertext.length()) {
+
+            while (key.length() < ciphertext.length()) {
+                key += key;
+            }
+        }
+
+        // Creates 2 int arrays that contain the index of each letter in the alphabet
+        int[] keyArr = key.chars().limit(ciphertext.length()).map(c -> c - 64).toArray();
+        int[] plaintextArr = ciphertext.chars().map(c -> c - 64).toArray();
+
+        int[] out = new int[plaintextArr.length];
+        // Here, the main decryption happens
+        for (int i = 0; i < ciphertext.length(); i++)
+            out[i] = plaintextArr[i] - keyArr[i] % 27;
+
+        return Arrays.stream(out)
+                .map(c -> c + 64) // mapping back to the ASCII representation
+                .mapToObj(c -> (char) c) // casting back to character
+                .map(String::valueOf) // maps every character to a String object
+                .collect(Collectors.joining()); // joins the string
+
+
+    }
+
+    //TODO implement
+    private static String encryptXOR(String ciphertext, String key) {
+
+        return null;
+    }
+
+    //TODO implement
+    private static String decryptXOR(String ciphertext, String key) {
+
+        return null;
+    }
+
+
+    public static String toBinary(String input){
+        StringBuilder builder = new StringBuilder();
+
+        for (char c : input.toCharArray()){
+
+            String binaryString = Integer.toBinaryString(c);
+
+            while (binaryString.length() < 8)
+                binaryString += "0";
+
+            builder.append(binaryString);
+        }
+
+        return builder.toString();
     }
 
 }
