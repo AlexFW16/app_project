44710b115ac48277b0d17855f6c4d09114da3d36
AlexFW <peherstorfer.alexander@gmail.com>
2024-01-06 21:24:15 +0000

    Changed XOR binary button to switch.

--- app/src/main/java/uk/bradford/app_project/fragments/XORFragment.java
+++ app/src/main/java/uk/bradford/app_project/fragments/XORFragment.java
@@ -5,8 +5,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 
 import uk.bradford.app_project.Cipher;
@@ -16,8 +16,7 @@ import uk.bradford.app_project.Util;
 
 public class XORFragment extends CipherFragment {
 
-    private boolean isBinaryOut;
-    private Button toggleBinaryButton;
+    private Switch toggleBinarySwitch;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -26,18 +25,17 @@ public class XORFragment extends CipherFragment {
 
         encryptBtn = rootView.findViewById(R.id.encrypt);
         decryptBtn = rootView.findViewById(R.id.decrypt);
-        toggleBinaryButton = rootView.findViewById(R.id.toggle_binary);
+        toggleBinarySwitch = rootView.findViewById(R.id.toggle_binary);
         keyEditText = rootView.findViewById(R.id.key);
         msgEditText = rootView.findViewById(R.id.msg);
         outputTextView = rootView.findViewById(R.id.out);
 
-        isBinaryOut = false;
 
         // Set the listeners
 
         encryptBtn.setOnClickListener(this::onEncryptButtonClick);
         decryptBtn.setOnClickListener(this::onDecryptButtonClick);
-        toggleBinaryButton.setOnClickListener(this::onToggleBinaryButtonClick);
+        toggleBinarySwitch.setOnCheckedChangeListener(this::onBinaryToggle);
 
         return rootView;
     }
@@ -49,7 +47,11 @@ public class XORFragment extends CipherFragment {
 
     private void onEncryptButtonClick(View v) {
         try {
+
             String encryptedText = Crypto.encrypt(Cipher.XOR, msgEditText.getText().toString(), keyEditText.getText().toString());
+            if (toggleBinarySwitch.isChecked())
+                encryptedText = Util.fromStringToBinaryString(encryptedText);
+
             outputTextView.setText(encryptedText);
         } catch (IllegalArgumentException e) {
 
@@ -62,6 +64,9 @@ public class XORFragment extends CipherFragment {
     private void onDecryptButtonClick(View v) {
         try {
             String decryptedText = Crypto.decrypt(Cipher.XOR, msgEditText.getText().toString(), keyEditText.getText().toString());
+
+            if (toggleBinarySwitch.isChecked())
+                decryptedText = Util.fromStringToBinaryString(decryptedText);
             outputTextView.setText(decryptedText);
         } catch (IllegalArgumentException e) {
             // TODO Toast
@@ -71,14 +76,13 @@ public class XORFragment extends CipherFragment {
 
     }
 
-    private void onToggleBinaryButtonClick(View v) {
+    private void onBinaryToggle(CompoundButton buttonView, boolean isChecked) {
 
-        if (isBinaryOut)
+        if (!isChecked)
             outputTextView.setText(Util.fromBinaryStringToString(outputTextView.getText().toString()));
         else
             outputTextView.setText(Util.fromStringToBinaryString(outputTextView.getText().toString()));
 
-        isBinaryOut = !isBinaryOut; // switch
 
     }
 
